8112b8e9214640decdadedc14c359df1e8ef9903
Matheus Aguiar <matheus.pit@gmail.com>
2024-07-11 16:36:10 -0300

    fixup! tests

--- test/libsolidity/semanticTests/variables/transient_state_variable.sol
+++ test/libsolidity/semanticTests/variables/transient_state_variable.sol
@@ -14,6 +14,7 @@ contract C {
     }
 }
 // ====
+// EVMVersion: >=cancun
 // compileViaYul: false
 // ----
 // x() -> 0
--- test/libsolidity/semanticTests/variables/transient_state_variable_initialization.sol
+++ test/libsolidity/semanticTests/variables/transient_state_variable_initialization.sol
@@ -7,6 +7,7 @@ contract C {
 
 }
 // ====
+// EVMVersion: >=cancun
 // compileViaYul: false
 // ----
 // constructor()
