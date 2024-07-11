524774a371afcebb5a3f4beec01b6e012e73245a
Matheus Aguiar <matheus.pit@gmail.com>
2024-07-11 16:03:27 -0300

    fixup! test

--- test/libsolidity/semanticTests/variables/transient_state_variable_initialization.sol
+++ test/libsolidity/semanticTests/variables/transient_state_variable_initialization.sol
@@ -1,11 +1,8 @@
 contract C {
-    uint transient public x;
+    uint transient public x = 2;
 
     constructor() {
         x = 1;
-    }
-    function f() public returns (uint) {
-        return x + 1;
     }
 
 }
@@ -13,7 +10,6 @@ contract C {
 // compileViaYul: false
 // ----
 // constructor()
-// gas legacy: 57728
+// gas legacy: 57897
 // gas legacy code: 57200
 // x() -> 0
-// f() -> 1
